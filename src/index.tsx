2ff17da1afc1e7c95e59edde9559bd8f3492b8a0
Jade Piramide <jadepiramidepogi@gmail.com>
2023-05-20 23:50:02 -0700

    feat: work on website routes to figure out the general skeleton of the website

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
+import Home from './components/Home';
+import OperatorList from './components/OperatorList';
+import Operator from './components/Operator';
+import OperatorSkills from './components/Operator/Skills';
+import OperatorSkill from './components/Operator/Skill';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +17,17 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/operators" element={<OperatorList />} />
+        <Route path="/operators/:operator" element={<Operator />}>
+          <Route path="skills" element={<OperatorSkills />}>
+            <Route path=":skill" element={<OperatorSkill />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>
 );
