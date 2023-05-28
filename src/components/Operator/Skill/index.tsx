3be72df73102fd79c0ac86fb101867012ce178d5
Jade Piramide <jadepiramidepogi@gmail.com>
2023-05-28 04:33:22 -0700

    fix: add useState and Link into imports to fix missing import errors

--- src/components/Operator/Skill/index.tsx
+++ src/components/Operator/Skill/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import './Skill.css';
+
+import {
+  SkillLevel,
+} from '../../../lib/OperatorData';
 
 function Skill({ level }) {
   const {operator, skill} = useParams();
