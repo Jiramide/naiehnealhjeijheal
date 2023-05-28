6bed379c145d6c8e3bcfd604f33b86e4e112abeb
Jade Piramide <jadepiramidepogi@gmail.com>
2023-05-28 07:02:32 -0700

    feat: added OperatorList rendering for very basic navigation testing (e.g. navigating to an operator yields the correct URL, and renders the operator)

--- src/components/OperatorList/index.tsx
+++ src/components/OperatorList/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
+import {
+  getOperators,
+} from '../../lib/OperatorData';
+import OperatorCard from './OperatorCard';
+
 import './OperatorList.css';
 
 function OperatorList({}) {
-  return (
-    <>
+  const operators = getOperators();
 
-    </>
+  return (
+    <div className="operator-list">
+      {
+        operators.map((operator) => {
+          return (
+            <OperatorCard
+              operator={operator}
+              key={operator.getId()}
+            />
+          );
+        })
+      }
+    </div>
   );
 }
 
