9677b431d603becb9e24a463a1e726d83a2400c7
Jade Piramide <jadepiramidepogi@gmail.com>
2023-05-28 07:04:50 -0700

    feat: added basic functionality to Skills component, listing all of the skills and providing links to those skills

--- src/components/Operator/Skills/index.tsx
+++ src/components/Operator/Skills/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+
+import useOperator from '../../../lib/OperatorHook';
 
 import './Skills.css';
 
+function SkillButton({ skill }) {
+  return (
+    <Link to={skill.getIndex().toString()}>
+      { skill.getName() }
+    </Link>
+  );
+}
+
 function Skills({}) {
+  const operator = useOperator();
+  const operatorSkills = operator.getSkills();
+
   return (
     <>
       <p>Viewing skills</p>
+      {
+        operatorSkills.map((skill) => {
+          return <SkillButton key={skill.getName()} skill={skill} />;
+        })
+      }
       <Outlet />
     </>
   );
