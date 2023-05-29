80575400cf121c32a33be1dcb32251521a7936bc
Jade Piramide <jadepiramidepogi@gmail.com>
2023-05-29 04:26:06 -0700

    refactor: lift operator-related type defns out of OperatorData and put it into its own file

--- src/lib/OperatorData.ts
+++ src/lib/OperatorData.ts
@@ -1,42 +1,4 @@
-export type OperatorId = string;
-export type OperatorName = string;
-export type OperatorImage = string;
-export type OperatorRarity = 1 | 2 | 3 | 4 | 5 | 6;
-
-export type SkillName = string;
-export type SkillDescription = string;
-export type SkillIcon = string;
-export type SkillIndex = 1 | 2 | 3;
-export type SkillLevel = 1 | 2 | 3 | 4 | 5 | 6 | 7 | "M1" | "M2" | "M3";
-
-export type Blackboard = Map<string, number>;
-
-export type Resource = number;
-export type ResourceUsage = Map<Resource, number>;
-
-export interface SkillLevelInfo {
-  getDescription(): SkillDescription,
-  getBlackboard(): Blackboard;
-  getMaterials(): ResourceUsage;
-};
-
-export interface Skill {
-  getName(): SkillName;
-  getIcon(): SkillIcon;
-  getIndex(): SkillIndex;
-  getLevel(skillLevel: SkillLevel): SkillLevelInfo;
-  getLevels(): SkillLevelInfo[];
-};
-
-export interface Operator {
-  getId(): OperatorName;
-  getName(): OperatorName;
-  getImage(): OperatorImage;
-  getRarity(): OperatorRarity;
-
-  getSkill(skillIndex: SkillIndex): Skill;
-  getSkills(): Skill[];
-};
+import * from './OperatorTypes';
 
 const operatorIds = ["skadi-31"];
 const operatorIdToName = {
@@ -72,7 +34,7 @@ export function getOperatorById(operatorId: OperatorId): Operator {
       getLevels: constant<SkillLevelInfo[]>([1, 2, 3, 4, 5, 6, 7, "M1", "M2", "M3"].map(getSkillLevel)),
     };
   }
-  const getSkills = (): Skill[] => [1, 2, 3].map(getSkill);
+  const getSkills = constant<Skill[]>([1, 2, 3].map(getSkill));
 
   return { getId, getName, getImage, getSkill, getRarity, getSkills };
 }
